1d11dd20c746638bc4da13700d21e0b42a0b44fe
Y0U5S3F <akim25486@gmail.com>
2024-02-18 16:47:36 +0100

    js cond

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-document.querySelector('#login').onclick = function(){
-    const name = document.querySelector('#name').value;
-    document.querySelector('#mtitle').innerHTML = ('Welcome {$.name}')
+document.getElementById('login').disabled = true ;    
+
+document.getElementById('username').onkeyup = function(){
+    if(document.getElementById('username').value.length > 0){
+        document.getElementById('login').disabled = false ;    
+    }
+    else{
+        document.getElementById('login').disabled = true ;    
+    }
 }
+
+document.getElementById('login').onclick = function() {
+    const val = document.getElementById('username').value;
+    document.getElementById('mtitle').innerHTML = `Welcome ${val} !`;
+    document.querySelector('#username').value = '';
+    document.getElementById('login').disabled = true ;    
+};
